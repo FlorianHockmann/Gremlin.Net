a6b1c028e018ea4e85ff1347f180586062b36d20
Florian Hockmann <fh@florian-hockmann.de>
2016-11-16 21:08:40 +0100

    Remove non-documentation files

--- Coverage.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location "C:\projects\gremlin-net\test\Gremlin.Net.UnitTest"
-dotnet test
-Set-Location "C:\projects\gremlin-net\test\Gremlin.Net.IntegrationTest"
-
-iex ((Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\OpenCover'))[0].FullName + '\tools\OpenCover.Console.exe' + ' -register:user -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test" -output:coverage.xml -skipautoprops -filter:"+[Gremlin*]*"')
-
-iex ((Get-ChildItem ($env:USERPROFILE + '\.nuget\packages\coveralls.io'))[0].FullName + '\tools\coveralls.net.exe' + ' --opencover coverage.xml')
