5da1a7b2f171137ef488a5495c1a2914b726d26f
Florian Hockmann <fh@florian-hockmann.de>
2016-12-06 19:48:48 +0100

    Add creation of enums

--- groovy/gremlin.csharp/src/main/groovy/GenerateGremlinCSharp.groovy
+++ groovy/gremlin.csharp/src/main/groovy/GenerateGremlinCSharp.groovy
@@ -2,9 +2,10 @@
 public class GenerateGremlinCSharp {
 
     public static void main(String[] args) {
-        String csharpDirectory = args[0];
-        GraphTraversalSourceGenerator.create(csharpDirectory + "/Process/" + "GraphTraversalSource.cs");
-        GraphTraversalGenerator.create(csharpDirectory + "/Process/" + "GraphTraversal.cs");
-        AnonymousTraversalGenerator.create(csharpDirectory + "/Process/" + "__.cs");
+        final String csharpDirectory = args[0]
+        GraphTraversalSourceGenerator.create(csharpDirectory + "/Process/" + "GraphTraversalSource.cs")
+        GraphTraversalGenerator.create(csharpDirectory + "/Process/" + "GraphTraversal.cs")
+        AnonymousTraversalGenerator.create(csharpDirectory + "/Process/" + "__.cs")
+        EnumGenerator.create(csharpDirectory + "/Process/")
     }
 }
