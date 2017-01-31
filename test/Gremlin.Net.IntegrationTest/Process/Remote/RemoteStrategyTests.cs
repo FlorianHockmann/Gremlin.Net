8717109790073a648e45e1346f6e3b74f79118d7
Florian Hockmann <fh@florian-hockmann.de>
2017-01-31 20:31:06 +0100

    Add simple integration test for RemoteStrategy

---
+++ test/Gremlin.Net.IntegrationTest/Process/Remote/RemoteStrategyTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Remote;
+using Gremlin.Net.Process.Remote;
+using Gremlin.Net.Process.Traversal;
+using Xunit;
+
+namespace Gremlin.Net.IntegrationTest.Process.Remote
+{
+    public class RemoteStrategyTests
+    {
+        private static readonly string TestHost = ConfigProvider.Configuration["TestServerIpAddress"];
+        private static readonly int TestPort = Convert.ToInt32(ConfigProvider.Configuration["TestServerPort"]);
+
+        [Fact]
+        public void RemoteStrategyShouldSendBytecodeToGremlinServer()
+        {
+            const string expectedResult = "gremlin";
+            var testBytecode = new Bytecode();
+            testBytecode.AddStep("V");
+            testBytecode.AddStep("inject", expectedResult);
+            var testTraversal = CreateTraversalWithRemoteStrategy(testBytecode);
+
+            var actualResult = testTraversal.Next();
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        private Traversal CreateTraversalWithRemoteStrategy(Bytecode bytecode)
+        {
+            var remoteStrategy =
+                new RemoteStrategy(new DriverRemoteConnection(new GremlinClient(new GremlinServer(TestHost, TestPort))));
+            return new TestTraversal(remoteStrategy, bytecode);
+        }
+    }
+
+    internal class TestTraversal : Traversal
+    {
+        public TestTraversal(ITraversalStrategy traversalStrategy, Bytecode bytecode)
+        {
+            TraversalStrategies.Add(traversalStrategy);
+            Bytecode = bytecode;
+        }
+    }
+}
