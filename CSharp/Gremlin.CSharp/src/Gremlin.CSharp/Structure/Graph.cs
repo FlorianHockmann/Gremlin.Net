26794836424a9035ac085dd688a8d4d3adb396b7
Florian Hockmann <fh@florian-hockmann.de>
2017-01-31 21:08:07 +0100

    Add src and test directories

---
+++ CSharp/Gremlin.CSharp/src/Gremlin.CSharp/Structure/Graph.cs
@@ -0,0 +1,12 @@
+using Gremlin.CSharp.Process;
+
+namespace Gremlin.CSharp.Structure
+{
+    public class Graph
+    {
+        public GraphTraversalSource traversal()
+        {
+            return new GraphTraversalSource();
+        }
+    }
+}
