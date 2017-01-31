8602efafad74e79402df2680f9ac9c5f6167853f
Florian Hockmann <fh@florian-hockmann.de>
2017-01-31 21:08:23 +0100

    Add first integration test

---
+++ CSharp/Gremlin.CSharp/test/Gremlin.CSharp.IntegrationTest/GraphTraversalTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Gremlin.CSharp.Process;
+using Gremlin.CSharp.Structure;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Remote;
+using Gremlin.Net.IntegrationTest;
+using Gremlin.Net.Process.Remote;
+using Xunit;
+
+namespace Gremlin.CSharp.IntegrationTest
+{
+    public class GraphTraversalTests
+    {
+        private static readonly string TestHost = ConfigProvider.Configuration["TestServerIpAddress"];
+        private static readonly int TestPort = Convert.ToInt32(ConfigProvider.Configuration["TestServerPort"]);
+
+        [Fact]
+        public void g_V_Count_Test()
+        {
+            var graph = new Graph();
+            var connection = CreateRemoteConnection();
+            var g = graph.traversal().WithRemote(connection);
+
+            var count = g.V().Count().Next();
+
+            Assert.Equal((long)0, count);
+        }
+
+        private IRemoteConnection CreateRemoteConnection()
+        {
+            return new DriverRemoteConnection(new GremlinClient(new GremlinServer(TestHost, TestPort)));
+        }
+    }
+}
---
+++ CSharp/Gremlin.CSharp/test/Gremlin.CSharp.IntegrationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gremlin.CSharp.IntegrationTest")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("232f0f2b-178e-4214-99c7-cc4dc6710f44")]
