00c164ffd628aa49abfda1d21260d03de1bf8b76
Florian Hockmann <fh@florian-hockmann.de>
2017-02-12 21:13:14 +0100

    Clean up old GraphSON serialization

--- src/Gremlin.Net/Structure/IO/GraphSON/NumberConverter.cs
+++ src/Gremlin.Net/Structure/IO/GraphSON/NumberConverter.cs
@@ -1,34 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Gremlin.Net.Structure.IO.GraphSON
 {
-    public abstract class NumberConverter : JsonConverter, IGraphSONDeserializer, IGraphSONSerializer
+    public abstract class NumberConverter : IGraphSONDeserializer, IGraphSONSerializer
     {
         protected abstract string GraphSONTypeName { get; }
         protected abstract Type HandledType { get; }
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            writer.WriteStartObject();
-            writer.WritePropertyName(GraphSONTokens.TypeKey);
-            writer.WriteValue($"{GraphSONTokens.GremlinTypeNamespace}:{GraphSONTypeName}");
-            writer.WritePropertyName(GraphSONTokens.ValueKey);
-            writer.WriteValue(value);
-            writer.WriteEndObject();
-        }
-
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public override bool CanConvert(Type objectType)
-        {
-            return objectType == HandledType;
-        }
 
         public dynamic Objectify(JToken graphsonObject, GraphSONReader reader)
         {
