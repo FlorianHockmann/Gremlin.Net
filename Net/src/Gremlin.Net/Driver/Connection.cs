aa4186f364ec84c39da22e91c04837af42f7ce63
Florian Hockmann <fh@florian-hockmann.de>
2017-03-06 21:05:12 +0100

    Remove unused imports

--- Net/src/Gremlin.Net/Driver/Connection.cs
+++ Net/src/Gremlin.Net/Driver/Connection.cs
@@ -17,10 +17,8 @@
 #endregion
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Gremlin.Net.Driver.Messages;
